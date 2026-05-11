3105025a40a3ad13574784e1b1a89241873500af
Kevin Ramos <182262851+Peti-sui@users.noreply.github.com>
2026-05-11 08:25:06 +0100

    Delete PROYECTO FINAL directory

--- PROYECTO FINAL/Pagina-Web/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Iniciar la sesion y destruir todos los datos de la misma */
-session_start();
-session_destroy();
-/* Redirigir al formulario de login */
-header('Location: login.php');
-exit();
-?>
